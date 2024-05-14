8a7f73621c8503fd606df4e00b138dae1c7ee9cc
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-05-14 23:47:29 +0600

    add--custom_circular_button.dart

---
+++ lib/view/screens/widgets/custom_circular_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class RoundedCircularButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const RoundedCircularButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.grey.withOpacity(.5),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
