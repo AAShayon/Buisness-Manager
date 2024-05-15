1a9e1121295123b988bd4ff0477fb6d61dd79464
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-05-15 22:57:52 +0600

    update---landing_screen.dart

--- lib/view/landing_screen.dart
+++ lib/view/landing_screen.dart
@@ -1,6 +1,9 @@
+import 'package:buisness_manager/modules/auth/view/login.dart';
+import 'package:buisness_manager/modules/auth/view_model/auth_view_model.dart';
 import 'package:buisness_manager/view/widget/custom_container.dart';
 import 'package:flutter/material.dart';
 import 'package:buisness_manager/modules/auth/model/core/response_model/logIn_response_model.dart';
+import 'package:provider/provider.dart';
 
 class LandingScreen extends StatefulWidget {
   final LogInResponseModel? logInResponseModel;
@@ -23,22 +26,44 @@ class _LandingScreenState extends State<LandingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return CustomContainer(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (user != null) ...[
-              Text('Name: ${user!.name ?? 'N/A'}', style: TextStyle(fontSize: 20)),
-              Text('Email: ${user!.email ?? 'N/A'}', style: TextStyle(fontSize: 20)),
-              Text('Phone: ${user!.phone ?? 'N/A'}', style: TextStyle(fontSize: 20)),
-              Text('Business Name: ${user!.businessName ?? 'N/A'}', style: TextStyle(fontSize: 20)),
-              // Add more user details as needed
-            ] else
-              Text('No user data available', style: TextStyle(fontSize: 20)),
-          ],
-        ),
-      ),
+    return Consumer<AuthViewModel>(
+      builder: (context,authViewModel,child) {
+        return CustomContainer(
+          appBar: AppBar(
+            backgroundColor: Colors.greenAccent,
+            title: Center(child: Text('Welcome, ${user?.name ?? "User"}')),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.logout),
+                onPressed: () async {
+                await authViewModel.logOut(context);
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => Login()),
+                    );
+
+
+                }
+              )
+            ],
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (user != null) ...[
+                  Text('Name: ${user!.name ?? 'N/A'}', style: TextStyle(fontSize: 20)),
+                  Text('Email: ${user!.email ?? 'N/A'}', style: TextStyle(fontSize: 20)),
+                  Text('Phone: ${user!.phone ?? 'N/A'}', style: TextStyle(fontSize: 20)),
+                  Text('Business Name: ${user!.businessName ?? 'N/A'}', style: TextStyle(fontSize: 20)),
+                  // Add more user details as needed
+                ] else
+                  Text('No user data available', style: TextStyle(fontSize: 20)),
+              ],
+            ),
+          ),
+        );
+      }
     );
   }
 }
