ae8292b82b33907a302accc1fe392131df3c7bcc
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-05-15 13:20:02 +0600

    add--auth_service.dart

---
+++ lib/modules/auth/model/service/remote/auth_service.dart
@@ -0,0 +1,67 @@
+
+
+import 'dart:developer';
+
+import 'package:buisness_manager/model/core/api_urls.dart';
+import 'package:buisness_manager/model/service/remote/dio_service.dart';
+import 'package:buisness_manager/modules/auth/model/core/request_model/VerifyOtpRequestModel.dart';
+import 'package:buisness_manager/modules/auth/model/core/request_model/logIn_request_model.dart';
+import 'package:buisness_manager/modules/auth/model/core/request_model/register_request_model.dart';
+import 'package:buisness_manager/modules/auth/model/core/request_model/resend_register_otp_request_model.dart';
+import 'package:buisness_manager/modules/auth/model/core/request_model/send_login_otp_request_model.dart';
+import 'package:dio/dio.dart';
+
+abstract class AuthService{
+  Future<Response> logInWithOtp(LogInRequestModel logInRequestModel);
+  Future<Response> register(RegisterRequestModel registerRequestModel);
+  Future<Response> verifyOtp(VerifyOtpRegisterRequestModel verifyOtpRequestModel);
+  Future<Response> resendOtpForRegister(ResendRegisterOtpRequestModel registerOtpRequestModel);
+  Future<Response> sendOtpForLogin(SendOtpRequestForLoginModel sendOtpRequestForLoginModel);
+  Future<Response> logOut();
+}
+
+class AuthRemoteDataSource extends AuthService{
+  static final AuthRemoteDataSource _singleton = AuthRemoteDataSource._internal();
+  final _dioService=DioService();
+  factory AuthRemoteDataSource(){
+  return _singleton;
+  }
+  AuthRemoteDataSource._internal();
+
+  @override
+  Future<Response> logInWithOtp(LogInRequestModel logInRequestModel) async{
+    Response? response= await _dioService.post(ApiUrl.login, data:logInRequestModel.toJson());
+    return response!;
+  }
+
+  @override
+  Future<Response> logOut() async{
+    Response? response= await _dioService.post(ApiUrl.logout);
+    return response!;
+  }
+
+  @override
+  Future<Response> register(RegisterRequestModel registerRequestModel) async{
+    Response? response= await _dioService.post(ApiUrl.registration,data: registerRequestModel.toJson());
+    return response!;
+  }
+
+  @override
+  Future<Response> resendOtpForRegister(ResendRegisterOtpRequestModel registerOtpRequestModel) async{
+    Response? response= await _dioService.post(ApiUrl.registration,data: registerOtpRequestModel.toJson());
+    return response!;
+  }
+
+  @override
+  Future<Response> sendOtpForLogin(SendOtpRequestForLoginModel sendLoginOtpRequestModel) async{
+    Response? response= await _dioService.post(ApiUrl.sendLoginOtp,data: sendLoginOtpRequestModel.toJson());
+    return response!;
+  }
+
+  @override
+  Future<Response> verifyOtp(VerifyOtpRegisterRequestModel verifyOtpRequestModel) async{
+    Response? response= await _dioService.post(ApiUrl.registration,data: verifyOtpRequestModel.toJson());
+    return response!;
+  }
+
+}
