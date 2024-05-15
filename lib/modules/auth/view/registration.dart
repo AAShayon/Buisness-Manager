0ab3d0dca6b961bec73a5afe863683672eb37924
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-05-15 22:56:07 +0600

    update---registration.dart

--- lib/modules/auth/view/registration.dart
+++ lib/modules/auth/view/registration.dart
@@ -1,4 +1,6 @@
-import 'package:buisness_manager/view/landing_screen.dart';
+import 'package:buisness_manager/modules/auth/model/core/request_model/register_request_model.dart';
+import 'package:buisness_manager/modules/auth/view_model/auth_view_model.dart';
+import 'package:buisness_manager/view/otp_sending.dart';
 import 'package:flutter/material.dart';
 import 'package:buisness_manager/view/widget/custom_circular_button.dart';
 import 'package:buisness_manager/view/widget/custom_container.dart';
@@ -6,6 +8,7 @@ import 'package:buisness_manager/view/widget/custom_text_from_filed.dart';
 import 'package:buisness_manager/view/widget/text_size.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
 
 class RegistrationPage extends StatefulWidget {
   const RegistrationPage({super.key});
@@ -33,147 +36,173 @@ class _RegistrationPageState extends State<RegistrationPage> {
 
   @override
   Widget build(BuildContext context) {
-    return CustomContainer(
-      child: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(child: HeadLineMediumText(text: 'Welcome to Registration', color: Colors.white)),
-              SizedBox(height: 25.h),
-              Container(
-                width: 350.w,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.deepPurple),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 16.h, horizontal: 16.w),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CustomTextFormField(
-                        hintText: 'Phone',
-                        prefixIcon: Iconsax.mobile,
-                        textInputTypeKeyboard: TextInputType.phone,
-                        controller: phoneNumberController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter a phone number';
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(height: 15.h),
-                      CustomTextFormField(
-                        hintText: 'Email',
-                        prefixIcon: Icons.email,
-                        textInputTypeKeyboard: TextInputType.emailAddress,
-                        controller: emailController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter an email';
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(height: 15.h),
-                      CustomTextFormField(
-                        hintText: 'Name',
-                        prefixIcon: Iconsax.user,
-                        textInputTypeKeyboard: TextInputType.text,
-                        controller: nameController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your name';
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(height: 15.h),
-                      CustomTextFormField(
-                        hintText: 'Business Name',
-                        prefixIcon: Iconsax.add,
-                        textInputTypeKeyboard: TextInputType.text,
-                        controller: businessNameController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter a business name';
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(height: 15.h),
-                      DropdownButtonFormField<String>(
-                        decoration: InputDecoration(
-                          labelText: 'Business Type',
-                          prefixIcon: Icon(Icons.add),
-                          border: OutlineInputBorder(),
-                        ),
-                        value: selectedBusinessType,
-                        items: [
-                          DropdownMenuItem<String>(
-                            value: '1',
-                            child: Text('Retailer/Shop'),
+    return Consumer<AuthViewModel>(
+      builder: (context,authViewModel,child) {
+        return CustomContainer(
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Center(child: HeadLineMediumText(text: 'Welcome to Registration', color: Colors.white)),
+                  SizedBox(height: 25.h),
+                  Container(
+                    width: 350.w,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.deepPurple),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 16.h, horizontal: 16.w),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CustomTextFormField(
+                            hintText: 'Phone',
+                            prefixIcon: Iconsax.mobile,
+                            textInputTypeKeyboard: TextInputType.phone,
+                            controller: phoneNumberController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter a phone number';
+                              }
+                              return null;
+                            },
                           ),
-                          DropdownMenuItem<String>(
-                            value: '2',
-                            child: Text('Wholesaler'),
+                          SizedBox(height: 15.h),
+                          CustomTextFormField(
+                            hintText: 'Email',
+                            prefixIcon: Icons.email,
+                            textInputTypeKeyboard: TextInputType.emailAddress,
+                            controller: emailController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter an email';
+                              }
+                              return null;
+                            },
                           ),
-                          DropdownMenuItem<String>(
-                            value: '3',
-                            child: Text('Distributor'),
+                          SizedBox(height: 15.h),
+                          CustomTextFormField(
+                            hintText: 'Name',
+                            prefixIcon: Iconsax.user,
+                            textInputTypeKeyboard: TextInputType.text,
+                            controller: nameController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your name';
+                              }
+                              return null;
+                            },
                           ),
-                          DropdownMenuItem<String>(
-                            value: '4',
-                            child: Text('Manufacturer'),
+                          SizedBox(height: 15.h),
+                          CustomTextFormField(
+                            hintText: 'Business Name',
+                            prefixIcon: Iconsax.add,
+                            textInputTypeKeyboard: TextInputType.text,
+                            controller: businessNameController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter a business name';
+                              }
+                              return null;
+                            },
                           ),
-                          DropdownMenuItem<String>(
-                            value: '6',
-                            child: Text('Services'),
-                          ),
-                          DropdownMenuItem<String>(
-                            value: '7',
-                            child: Text('Other'),
+                          SizedBox(height: 15.h),
+                          DropdownButtonFormField<String>(
+                            decoration: const InputDecoration(
+                              labelText: 'Business Type',
+                              prefixIcon: Icon(Icons.add),
+                              border: OutlineInputBorder(),
+                            ),
+                            value: selectedBusinessType,
+                            items: const [
+                              DropdownMenuItem<String>(
+                                value: '1',
+                                child: Text('Retailer/Shop'),
+                              ),
+                              DropdownMenuItem<String>(
+                                value: '2',
+                                child: Text('Wholesaler'),
+                              ),
+                              DropdownMenuItem<String>(
+                                value: '3',
+                                child: Text('Distributor'),
+                              ),
+                              DropdownMenuItem<String>(
+                                value: '4',
+                                child: Text('Manufacturer'),
+                              ),
+                              DropdownMenuItem<String>(
+                                value: '6',
+                                child: Text('Services'),
+                              ),
+                              DropdownMenuItem<String>(
+                                value: '7',
+                                child: Text('Other'),
+                              ),
+                            ],
+                            onChanged: (value) {
+                              setState(() {
+                                selectedBusinessType = value;
+                              });
+                            },
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please select a business type';
+                              }
+                              return null;
+                            },
                           ),
                         ],
-                        onChanged: (value) {
-                          setState(() {
-                            selectedBusinessType = value;
-                          });
-                        },
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please select a business type';
-                          }
-                          return null;
-                        },
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(height: 15.h),
-              RoundedCircularButton(
-                text: 'Next',
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
+                  SizedBox(height: 15.h),
+                  authViewModel.isLoadingState?const CircularProgressIndicator(
+                    color: Colors.amber,
+                  ):RoundedCircularButton(
+                    text: 'Next',
+                    onPressed: () async {
 
-                    Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(builder: (context) => LandingScreen()),
-                          (route) => false,
-                    );
-                  }
-                },
+                      if (_formKey.currentState!.validate()) {
+                        // Navigator.pushAndRemoveUntil(
+                        //   context,
+                        //   MaterialPageRoute(builder: (context) => OtpScreen(identifier: registerRequestResponseModel.identifierId)),
+                        //       (route) => false,
+                        // );
+                        final registerRequestModel=RegisterRequestModel(
+                          phone: phoneNumberController.text,
+                          email: emailController.text,
+                          name: nameController.text,
+                          businessName: businessNameController.text,
+                          businessTypeId: selectedBusinessType!,
+                        );
+                        authViewModel.registration(registerRequestModel).then((isRegistered) {
+                          if(isRegistered){
+                            Navigator.push(context, MaterialPageRoute(builder: (context)=>OtpScreen(identifier: authViewModel.registerRequestResponseModel!.identifierId, isLoginPage: false,)));
+                          }
+                          else{
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Phone number or email is already registered'),
+                                duration: Duration(seconds: 3),
+                              ),
+                            );
+                          }
+                        });
+                      }
+                    },
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      }
     );
   }
 }
