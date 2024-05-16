c7199c45592fe496528c79d7c9f5253751a2f0c2
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-05-16 20:04:39 +0600

    add---common_use_container.dart

---
+++ lib/view/widget/common_use_container.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+class CommonUseContainer extends StatelessWidget {
+  final Widget child;
+  final double height;
+  final double width;
+  final Color color;
+
+   CommonUseContainer({super.key, required this.child, this.height= 100,  this.width = 100,  this.color=Colors.transparent});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.blue,width: 2.0),
+      ),
+      child: Padding(padding:  EdgeInsets.symmetric(horizontal: 5.w,vertical: 10.h),child: child,),
+    );
+  }
+}
