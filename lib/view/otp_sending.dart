f6b7d1b6ae0ae9e3f132567075605ee226a59152
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-05-16 20:05:43 +0600

    update---otp_sending.dart

--- lib/view/otp_sending.dart
+++ lib/view/otp_sending.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:buisness_manager/view/otp_recive.dart';
-import 'package:buisness_manager/view/widget/custom_container.dart';
+import 'package:buisness_manager/view/widget/custom_main_use_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
