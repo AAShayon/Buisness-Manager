4e5cc0ae9a28a2d83703171a3df7484ee46e7c97
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-05-18 00:04:07 +0600

    Update --- user_profile_update_form.dart

--- lib/modules/admin/view/widget/user_profile_update_form.dart
+++ lib/modules/admin/view/widget/user_profile_update_form.dart
@@ -1,3 +1,5 @@
+import 'package:buisness_manager/modules/admin/model/core/request_model/user_profile_update_request_model.dart';
+import 'package:buisness_manager/modules/admin/viewModel/user_profile_view_model.dart';
 import 'package:buisness_manager/modules/auth/viewModel/auth_view_model.dart';
 import 'package:buisness_manager/view/landing_screen.dart';
 import 'package:buisness_manager/view/widget/custom_circular_button.dart';
@@ -16,131 +18,179 @@ class UserProfileUpdateForm extends StatefulWidget {
 }
 
 class _UserProfileUpdateFormState extends State<UserProfileUpdateForm> {
-  final _formKey2 = GlobalKey<FormState>();
-  final TextEditingController nameController=TextEditingController();
-  final TextEditingController phoneNumberController=TextEditingController();
+  final _userProfileUpdateForm = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController phoneNumberController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
   String? selectedBusinessType;
   final RegExp phoneRegex = RegExp(r'^\+?(88)?0[1-9]\d{8,9}$');
+
   @override
-  void dispose(){
+  void dispose() {
     nameController.dispose();
     phoneNumberController.dispose();
+    emailController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-    final authViewModel=Provider.of<AuthViewModel>(context);
-    return CustomContainer(child:Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(height: 25.h,),
-        HeadlineLargeText(text: 'User Profile Update', color: Colors.white),
-        Padding(
-          padding:  EdgeInsets.symmetric(vertical: 10.h,horizontal: 10.w),
-          child: Container(
-            width: 350.w,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.deepPurple),
-              borderRadius: BorderRadius.circular(10),),
-            child: Column(
-              children: [
-                Padding(
-                  padding:  EdgeInsets.symmetric(horizontal: 5.h,vertical: 5.h),
-                  child: CustomTextFormField(
-                    controller: nameController,
-                      hintText: 'Name',
-                    prefixIcon: Icons.update,
-                    readOnly: false, validator: (value){
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter Valid  Name';
-                        }
-                        return null;
-                      }),
-                ),
-                Padding(
-                  padding:  EdgeInsets.symmetric(horizontal: 5.h,vertical: 5.h),
-                  child: CustomTextFormField(
-                      controller: phoneNumberController,
-                      hintText: 'Phone',
-                      prefixIcon: Icons.phone,
-                      readOnly: false,
-                      validator: (value){
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter a phone number';
-                        }
-                        if (!phoneRegex.hasMatch(value)) {
-                          return 'Please enter a valid phone number';
-                        }
-                        return null;
-                  }
-
-                  ),
-                ),
-
-                Padding(
-                  padding:  EdgeInsets.symmetric(vertical: 15.h,horizontal: 20.w),
-                  child: DropdownButtonFormField<String>(
-                    decoration: const InputDecoration(
-                      labelText: 'Business Type',
-                      prefixIcon: Icon(Icons.add),
-                      border: OutlineInputBorder(),
+    final userProfileViewModel = Provider.of<UserProfileViewModel>(context);
+    return CustomContainer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 25.h),
+          const HeadlineLargeText(text: 'User Profile Update', color: Colors.white),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
+            child: Container(
+              width: 350.w,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.deepPurple),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Form(
+                key: _userProfileUpdateForm,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 5.h, vertical: 5.h),
+                      child: CustomTextFormField(
+                        controller: nameController,
+                        hintText: 'Name',
+                        prefixIcon: Icons.update,
+                        readOnly: false,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a valid Name';
+                          }
+                          return null;
+                        },
+                      ),
                     ),
-                    value: selectedBusinessType,
-                    items: const [
-                      DropdownMenuItem<String>(
-                        value: '1',
-                        child: Text('Retailer/Shop'),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 5.h, vertical: 5.h),
+                      child: CustomTextFormField(
+                        controller: phoneNumberController,
+                        hintText: 'Phone',
+                        prefixIcon: Icons.phone,
+                        readOnly: false,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a phone number';
+                          }
+                          if (!phoneRegex.hasMatch(value)) {
+                            return 'Please enter a valid phone number';
+                          }
+                          return null;
+                        },
                       ),
-                      DropdownMenuItem<String>(
-                        value: '2',
-                        child: Text('Wholesaler'),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15.h, horizontal: 20.w),
+                      child: DropdownButtonFormField<String>(
+                        decoration: const InputDecoration(
+                          labelText: 'Business Type',
+                          prefixIcon: Icon(Icons.add),
+                          border: OutlineInputBorder(),
+                        ),
+                        value: selectedBusinessType,
+                        items: const [
+                          DropdownMenuItem<String>(
+                            value: '1',
+                            child: Text('Retailer/Shop'),
+                          ),
+                          DropdownMenuItem<String>(
+                            value: '2',
+                            child: Text('Wholesaler'),
+                          ),
+                          DropdownMenuItem<String>(
+                            value: '3',
+                            child: Text('Distributor'),
+                          ),
+                          DropdownMenuItem<String>(
+                            value: '4',
+                            child: Text('Manufacturer'),
+                          ),
+                          DropdownMenuItem<String>(
+                            value: '6',
+                            child: Text('Services'),
+                          ),
+                          DropdownMenuItem<String>(
+                            value: '7',
+                            child: Text('Other'),
+                          ),
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            selectedBusinessType = value;
+                          });
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please select a business type';
+                          }
+                          return null;
+                        },
                       ),
-                      DropdownMenuItem<String>(
-                        value: '3',
-                        child: Text('Distributor'),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 5.h, vertical: 5.h),
+                      child: CustomTextFormField(
+                        controller: emailController,
+                        hintText: 'Email',
+                        prefixIcon: Icons.email,
+                        readOnly: false,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter email';
+                          }
+                          return null;
+                        },
                       ),
-                      DropdownMenuItem<String>(
-                        value: '4',
-                        child: Text('Manufacturer'),
-                      ),
-                      DropdownMenuItem<String>(
-                        value: '6',
-                        child: Text('Services'),
-                      ),
-                      DropdownMenuItem<String>(
-                        value: '7',
-                        child: Text('Other'),
-                      ),
-                    ],
-                    onChanged: (value) {
-                      setState(() {
-                        selectedBusinessType = value;
-                      });
-                    },
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please select a business type';
-                      }
-                      return null;
-                    },
-                  ),
+                    ),
+                  ],
                 ),
-
-              ],
+              ),
             ),
           ),
-        ),
-        Padding(
-          padding:  EdgeInsets.symmetric(horizontal: 10.w),
-          child: CustomCircularButton(text: 'Update', onPressed: (){
-            if(_formKey2.currentState!.validate()){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=> LandingScreen(logInResponseModel: authViewModel.logInResponseModel!,)));
-            }
-          }),
-        ),
-      ],
-    ), );
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.w),
+            child: userProfileViewModel.isLoadingState
+                ? const CircularProgressIndicator(
+              color: Colors.amber,
+            )
+                : CustomCircularButton(
+              text: 'Update',
+              onPressed: () {
+                if (_userProfileUpdateForm.currentState!.validate()) {
+                  final userProfileUpdateRequestModel = UserProfileUpdateRequestModel(
+                    name: nameController.text,
+                    phone: phoneNumberController.text,
+                    email: emailController.text,
+                    businessTypeId: selectedBusinessType!,
+                  );
+                  userProfileViewModel.userProfileUpdateRequest(userProfileUpdateRequestModel).then((isUpdate) {
+                    if (isUpdate) {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const LandingScreen()));
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Phone number or email is needed to update'),
+                          duration: Duration(seconds: 3),
+                        ),
+                      );
+                    }
+                  });
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
