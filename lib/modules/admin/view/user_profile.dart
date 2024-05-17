ea70558c7585c2e5ed09b2cc58ecebf39f5bec11
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-05-17 23:20:41 +0600

    update --- user_profile.dart

--- lib/modules/admin/view/user_profile.dart
+++ lib/modules/admin/view/user_profile.dart
@@ -1,81 +1,69 @@
+import 'package:buisness_manager/modules/admin/model/core/response_model/user_profile_data_response_model.dart';
+import 'package:buisness_manager/modules/admin/view/widget/user_profile_card.dart';
 import 'package:buisness_manager/modules/admin/view/widget/user_profile_update_form.dart';
+import 'package:buisness_manager/modules/admin/viewModel/user_profile_view_model.dart';
 import 'package:buisness_manager/view/widget/custom_circular_button.dart';
 import 'package:buisness_manager/view/widget/custom_main_use_container.dart';
-import 'package:buisness_manager/view/widget/custom_text_from_filed.dart';
 import 'package:buisness_manager/view/widget/text_size.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
+
 class UserProfile extends StatelessWidget {
   const UserProfile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomContainer(child: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 25.h,),
-           HeadlineLargeText(text: 'UserName', color: Colors.white),
-          Padding(
-            padding:  EdgeInsets.symmetric(vertical: 10.h,horizontal: 10.w),
-            child: Container(
-              width: 350.w,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.deepPurple),
-                borderRadius: BorderRadius.circular(10),),
+    return Consumer<UserProfileViewModel>(
+      builder: (context, userProfileViewModel, child) {
+        final user = userProfileViewModel.responseUser;
+        if (user != null) {
+          return CustomContainer(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(20.w),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Padding(
-                    padding:  EdgeInsets.symmetric(horizontal: 5.h,vertical: 5.h),
-                    child: CustomTextFormField(hintText: 'ID', prefixIcon: Icons.perm_identity,readOnly: true,),
+                  SizedBox(height: 25.h),
+                  CircleAvatar(
+                    radius: 50.r,
+                    backgroundImage: const NetworkImage( 'https://via.placeholder.com/150'),
                   ),
-                  Padding(
-                    padding:  EdgeInsets.symmetric(horizontal: 5.h,vertical: 5.h),
-                    child: CustomTextFormField(hintText: 'Email', prefixIcon: Icons.perm_identity,readOnly: true,),
-                  ),
-                  Padding(
-                    padding:  EdgeInsets.symmetric(horizontal: 5.h,vertical: 5.h),
-                    child: CustomTextFormField(hintText: 'phone', prefixIcon: Icons.perm_identity,readOnly: true,),
-                  ),
-                  Padding(
-                    padding:  EdgeInsets.symmetric(horizontal: 5.h,vertical: 5.h),
-                    child: CustomTextFormField(hintText: 'Business Name', prefixIcon: Icons.perm_identity,readOnly: true,),
-                  ),Padding(
-                    padding:  EdgeInsets.symmetric(horizontal: 5.h,vertical: 5.h),
-                    child: CustomTextFormField(hintText: 'Business Type', prefixIcon: Icons.perm_identity,readOnly: true,),
-                  ),
-                  Padding(
-                    padding:  EdgeInsets.symmetric(horizontal: 5.h,vertical: 5.h),
-                    child: CustomTextFormField(hintText: 'Business Type ID', prefixIcon: Icons.perm_identity,readOnly: true,),
-                  ),
-                  Padding(
-                    padding:  EdgeInsets.symmetric(horizontal: 5.h,vertical: 5.h),
-                    child: CustomTextFormField(hintText: 'Company ID', prefixIcon: Icons.perm_identity,readOnly: true,),
-                  ),
-                  Padding(
-                    padding:  EdgeInsets.symmetric(horizontal: 5.h,vertical: 5.h),
-                    child: CustomTextFormField(hintText: 'Branch ID', prefixIcon: Icons.perm_identity,readOnly: true,),
+                  SizedBox(height: 15.h),
+                  HeadlineLargeText(text: user.name, color: Colors.lightBlueAccent),
+                  SizedBox(height: 10.h),
+                  UserProfileInfoCard(user: user),
+                  SizedBox(height: 20.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomCircularButton(
+                        text: 'Update',
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => UserProfileUpdateForm()),
+                          );
+                        },
+                      ),
+                      SizedBox(width: 20.w),
+                      CustomCircularButton(
+                        text: 'Delete',
+                        onPressed: () {
+                          // Add delete functionality here
+                        },
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding:  EdgeInsets.symmetric(horizontal: 10.w),
-                child: CustomCircularButton(text: 'Update', onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>UserProfileUpdateForm()));
-                }),
-              ),
-              CustomCircularButton(text: 'Delete', onPressed: (){}),
-            ],
-          ),
-        ],
-      ),
-    ));
+          );
+        }
+        return const Center(child: Text('No Data', style: TextStyle(fontSize: 18.0)));
+      },
+    );
   }
 }
+
+
