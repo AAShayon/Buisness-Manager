29b1f1fbe1a73229c2b5bebc6d7c02d9aa03b72d
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-06-02 09:02:58 +0600

    update-- user_profile_service.dart

--- lib/modules/admin/model/core/service/remote/user_profile_service.dart
+++ lib/modules/admin/model/core/service/remote/user_profile_service.dart
@@ -1,6 +1,8 @@
 import 'package:buisness_manager/model/core/api_urls.dart';
+import 'package:buisness_manager/model/service/local/shared_pre_service.dart';
 import 'package:buisness_manager/model/service/remote/dio_service.dart';
 import 'package:buisness_manager/modules/admin/model/core/request_model/user_profile_update_request_model.dart';
+import 'package:buisness_manager/modules/admin/model/core/response_model/user_profile_data_response_model.dart';
 import 'package:dio/dio.dart';
 
 abstract class UserProfileService{
@@ -8,11 +10,14 @@ abstract class UserProfileService{
   Future<Response> userProfile();
   Future<Response> userProfileUpdate(UserProfileUpdateRequestModel userProfileUpdateRequestModel);
   Future<Response> userDeleteProfile();
-  
+  Future<void> saveUser(ResponseUser? responseUser);
+  Future<void> clearUser();
+
 }
 
 class UserProfileDataSource extends UserProfileService{
   static final UserProfileDataSource _singleton= UserProfileDataSource._internal();
+  SharedPreService _sharedPreService=SharedPreService();
   final _dioService=DioService();
   factory UserProfileDataSource(){
     return _singleton;
@@ -35,6 +40,18 @@ class UserProfileDataSource extends UserProfileService{
   Future<Response> userDeleteProfile() async {
    Response? response=await _dioService.request(ApiUrl().userAccountDelete);
    return response!;
+  }
+
+  @override
+  Future<void> clearUser()async {
+    await _sharedPreService.delete(key: 'responseUser');
+
+
+  }
+
+  @override
+  Future<void> saveUser(ResponseUser? responseUser) async {
+    await _sharedPreService.write(key: 'responseUser', value: responseUser!.toJson());
   }
 
 
