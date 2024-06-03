5242f4e1aea682d20cbbf86deab1083ce38e0e40
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-06-03 17:00:12 +0600

    update -customer/view

--- lib/modules/customer/view/customer_supplier_list_screen.dart
+++ lib/modules/customer/view/customer_supplier_list_screen.dart
@@ -4,12 +4,13 @@ import 'package:buisness_manager/modules/customer/view/widget/customer_supplier_
 import 'package:buisness_manager/modules/customer/viewModel/customer_view_model.dart';
 import 'package:buisness_manager/modules/transaction/view/transaction_screen.dart';
 
-
 import 'package:buisness_manager/view/widget/custom_circular_button.dart';
 import 'package:buisness_manager/view/widget/custom_container.dart';
+import 'package:buisness_manager/view/widget/no_internet_widget.dart';
 import 'package:buisness_manager/view/widget/text_size.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:provider/provider.dart';
 
 class CustomerSupplierViewScreen extends StatefulWidget {
@@ -17,14 +18,19 @@ class CustomerSupplierViewScreen extends StatefulWidget {
   final String branchId;
   final int customerOrSupplierType;
 
-  CustomerSupplierViewScreen({super.key, this.name, required this.branchId, required this.customerOrSupplierType});
+  CustomerSupplierViewScreen(
+      {super.key,
+      this.name,
+      required this.branchId,
+      required this.customerOrSupplierType});
 
   @override
-  State<CustomerSupplierViewScreen> createState() => _CustomerSupplierViewScreenState();
+  State<CustomerSupplierViewScreen> createState() =>
+      _CustomerSupplierViewScreenState();
 }
 
-class _CustomerSupplierViewScreenState extends State<CustomerSupplierViewScreen> {
-
+class _CustomerSupplierViewScreenState
+    extends State<CustomerSupplierViewScreen> {
   @override
   void initState() {
     super.initState();
@@ -35,96 +41,151 @@ class _CustomerSupplierViewScreenState extends State<CustomerSupplierViewScreen>
 
   Future _loadData() async {
     final customerViewModel = Provider.of<CustomerViewModel>(context, listen: false);
-    await customerViewModel.customerListFetch(branchId: widget.branchId, customerOrSupplierType: widget.customerOrSupplierType,context);
+    await customerViewModel.customerListFetch(
+        branchId: widget.branchId,
+        customerOrSupplierType: widget.customerOrSupplierType,
+        context);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: RefreshIndicator(
-        onRefresh: () async {
-          _loadData();
-        },
-        child: CustomContainer(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(height: 50.h,),
-                HeadlineLargeText(text: widget.name ?? 'No Name', color: Colors.white),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 15.h),
-                  child: CustomCircularButton(text: 'Add Customer / Supplier', onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => CustomerOrSupplierCreate(id:widget.branchId,)));
-                  }),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10.h),
-                  child: HeadLineMediumText(text: widget.customerOrSupplierType == 0 ? 'Customer List' : 'Supplier List', color: Colors.white,),
-                ),
-                Consumer<CustomerViewModel>(
-                  builder: (context, customerViewModel, child) {
-                    final customers = customerViewModel.customers?.customerList;
-                    if (customerViewModel.isLoadingState) {
-                      return CircularProgressIndicator(color: Colors.green);
-                    } else if (customers == null) {
-                      return Text('No data');
-                    } else {
-                      return SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: DataTable(
-                          columnSpacing: 10.0, // Set column spacing to minimum
-                          columns: const [
-                            DataColumn(label: Text('ID')),
-                            DataColumn(label: Text('Name')),
-                            DataColumn(label: Text('Phone')),
-                            DataColumn(label: Text('Actions')),
-                          ],
-                          rows: customers.map((customer) {
-                            return DataRow(cells: [
-                              DataCell(Text('${customer.id}')),
-                              DataCell(Text('${customer.name}')),
-                              DataCell(Text('${customer.phone}')),
-                              DataCell(
-                                Row(
-                                  children: [
-                                    IconButton(
-                                      icon: Icon(Icons.remove_red_eye_rounded),
-                                      onPressed: () {
-                                        Navigator.push(context, MaterialPageRoute(builder: (context) => TransactionScreen(customerSupplierID: customer.id.toString(), branchID:widget.branchId,)));
-                                      },
-                                    ),
-                                    IconButton(
-                                      icon: Icon(Icons.edit),
-                                      onPressed: () {
-                                        Navigator.push(context, MaterialPageRoute(builder: (context) => CustomerOrSupplierUpdate(branchId: widget.branchId, customerOrSupplierId: customer.id.toString())));
-                                      },
-                                    ),
-                                    IconButton(
-                                      icon: Icon(Icons.delete),
-                                      onPressed: () async {
-                                        final customerViewModel = Provider.of<CustomerViewModel>(context, listen: false);
-                                        await customerViewModel.deleteCustomer(context, branchId: widget.branchId, customerOrSupplierId: customer.id.toString()).then((isDeleted)async {
-                                          if (isDeleted) {
-                                            await customerViewModel.customerListFetch(context, branchId: widget.branchId, customerOrSupplierType: widget.customerOrSupplierType);
-                                          }
-                                        });
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ]);
-                          }).toList(),
+    return Provider.of<InternetConnectionStatus>(context) == InternetConnectionStatus.disconnected
+        ? NoInternetWidget(
+            onPressed: () {
+              _loadData();
+            },
+          )
+        : Scaffold(
+            body: RefreshIndicator(
+              onRefresh: () async {
+               await _loadData();
+              },
+              child: CustomContainer(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 50.h,
+                      ),
+                      HeadlineLargeText(
+                          text: widget.name ?? 'No Name', color: Colors.white),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 15.h),
+                        child: CustomCircularButton(
+                            text: widget.customerOrSupplierType == 0
+                                ? 'Add Customer'
+                                : 'Add Supplier',
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => CustomerOrSupplierCreate(
+                                            id: widget.branchId,)));}),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10.h),
+                        child: HeadLineMediumText(
+                          text: widget.customerOrSupplierType == 0
+                              ? 'Customer List'
+                              : 'Supplier List',
+                          color: Colors.white,
                         ),
-                      );
-                    }
-                  },
+                      ),
+                      Consumer<CustomerViewModel>(
+                        builder: (context, customerViewModel, child) {
+                          final customers =
+                              customerViewModel.customers?.customerList;
+                          if (customerViewModel.isLoadingState) {
+                            return CircularProgressIndicator(
+                                color: Colors.green);
+                          } else if (customers == null) {
+                            return Text('No data');
+                          } else {
+                            return SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: DataTable(
+                                columnSpacing:
+                                    10.0, // Set column spacing to minimum
+                                columns: const [
+                                  DataColumn(label: Text('ID')),
+                                  DataColumn(label: Text('Name')),
+                                  DataColumn(label: Text('Phone')),
+                                  DataColumn(label: Text('Actions')),
+                                ],
+                                rows: customers.map((customer) {
+                                  return DataRow(cells: [
+                                    DataCell(Text('${customer.id}')),
+                                    DataCell(Text('${customer.name}')),
+                                    DataCell(Text('${customer.phone}')),
+                                    DataCell(
+                                      Row(
+                                        children: [
+                                          IconButton(
+                                            icon: Icon(
+                                                Icons.remove_red_eye_rounded),
+                                            onPressed: () {
+                                              Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(builder: (context) => TransactionScreen(customerSupplierID: customer.id.toString(), branchID: widget.branchId, customerSupplierType: widget.customerOrSupplierType,
+                                                          )));
+                                            },
+                                          ),
+                                          IconButton(
+                                            icon: Icon(Icons.edit),
+                                            onPressed: () {
+                                              Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          CustomerOrSupplierUpdate(
+                                                              branchId: widget
+                                                                  .branchId,
+                                                              customerOrSupplierId:
+                                                                  customer.id
+                                                                      .toString())));
+                                            },
+                                          ),
+                                          IconButton(
+                                            icon: Icon(Icons.delete),
+                                            onPressed: () async {
+                                              final customerViewModel = Provider
+                                                  .of<CustomerViewModel>(
+                                                      context,
+                                                      listen: false);
+                                              await customerViewModel
+                                                  .deleteCustomer(context,
+                                                      branchId: widget.branchId,
+                                                      customerOrSupplierId:
+                                                          customer.id
+                                                              .toString())
+                                                  .then((isDeleted) async {
+                                                if (isDeleted) {
+                                                  await customerViewModel
+                                                      .customerListFetch(
+                                                          context,
+                                                          branchId:
+                                                              widget.branchId,
+                                                          customerOrSupplierType:
+                                                              widget
+                                                                  .customerOrSupplierType);
+                                                }
+                                              });
+                                            },
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ]);
+                                }).toList(),
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
-    );
+          );
   }
 }
