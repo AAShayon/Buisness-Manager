592abb774ffb252210c8a464cab0e3b5d1b54ee0
Asif Afroj Shayon <shayon.asif@gmail.com>
2024-06-03 19:03:27 +0600

    update -login.dart

--- lib/modules/auth/view/login.dart
+++ lib/modules/auth/view/login.dart
@@ -35,75 +35,77 @@ class _LoginState extends State<Login> {
       body: Consumer<AuthViewModel>(
         builder: (context, authViewModel, child) {
           return CustomContainer(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const HeadlineLargeText(
-                  text: 'Buisness Manager',
-                  color: Colors.blueAccent,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 25.h),
-                  child: const HeadlineLargeText(
-                    text: 'Login',
-                    color: Colors.white,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(padding: EdgeInsets.symmetric(vertical: 15.h),
+                  child: Image.asset("assets/businessManager.png",fit: BoxFit.fill,),
                   ),
-                ),
-                CustomTextFormField(
-                  hintText: 'Mobile',
-                  prefixIcon: Iconsax.mobile,
-                  textInputTypeKeyboard: TextInputType.phone,
-                  controller: phoneNumberController,
-                ),
-                SizedBox(
-                  height: 25.h,
-                ),
-                authViewModel.isLoadingState
-                    ? const CircularProgressIndicator(
-                  color: Colors.green,
-                )
-                    : CustomCircularButton(
-                    text: 'Next',
-                    onPressed: () async {
-                      if (!authViewModel.isLoadingState) {
-                        await authViewModel.sendOtpForLogin(SendOtpRequestForLoginModel(identifier: phoneNumberController.text), context,).then((value) {
-                          if (value == true) {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => OtpScreen(
-                                  identifier: phoneNumberController.text,
-                                  isLoginPage: true,
-                                )));
-                          }
-                        });
-                      }
-                    }),
-                SizedBox(
-                  height: 25.h,
-                ),
-                TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const RegistrationPage()));
-                    },
-                    child: HeadLineSmallText(
-                        text:
-                        "If you don't have an account yet,please Sign Up",
-                        color: Colors.white)),
-                SizedBox(
-                  height: 25.h,
-                ),
-                CustomCircularButton(
-                    text: 'Signup',
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const RegistrationPage()));
-                    })
-              ],
+              
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 25.h),
+                    child: const HeadlineLargeText(
+                      text: 'Login',
+                      color: Colors.white,
+                    ),
+                  ),
+                  CustomTextFormField(
+                    hintText: 'Mobile',
+                    prefixIcon: Iconsax.mobile,
+                    textInputTypeKeyboard: TextInputType.phone,
+                    controller: phoneNumberController,
+                  ),
+                  SizedBox(
+                    height: 25.h,
+                  ),
+                  authViewModel.isLoadingState
+                      ? const CircularProgressIndicator(
+                    color: Colors.green,
+                  )
+                      : CustomCircularButton(
+                      text: 'Next',
+                      onPressed: () async {
+                        if (!authViewModel.isLoadingState) {
+                          await authViewModel.sendOtpForLogin(SendOtpRequestForLoginModel(identifier: phoneNumberController.text), context,).then((value) {
+                            if (value == true) {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => OtpScreen(
+                                    identifier: phoneNumberController.text,
+                                    isLoginPage: true,
+                                  )));
+                            }
+                          });
+                        }
+                      }),
+                  SizedBox(
+                    height: 25.h,
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const RegistrationPage()));
+                      },
+                      child: HeadLineSmallText(
+                          text:
+                          "If you don't have an account yet,please Sign Up",
+                          color: Colors.white)),
+                  SizedBox(
+                    height: 25.h,
+                  ),
+                  CustomCircularButton(
+                      text: 'Signup',
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const RegistrationPage()));
+                      })
+                ],
+              ),
             ),
           );
         },
